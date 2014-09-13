178b977c0b33d7c68f62bc08f6883a9d62682dd0
Naoto Takeyama <mt.bamboon@gmail.com>
2014-09-13 11:14:52 +0900

    Fix navbar

--- app/views/nav.blade.php
+++ app/views/nav.blade.php
@@ -9,7 +9,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Hao</a>
+			<a class="navbar-brand" href="#">Spologics</a>
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
